46921dc2945b4ebf06470c6a08e6558247d45b21
daillie <joker53412@mail.ru>
2024-06-12 16:04:21 +0400

    optimized database container and created non-root user

--- dbinit.sql
+++ dbinit.sql
@@ -1,5 +1,5 @@
 SET foreign_key_checks = 0;
-drop table if exists Attached_tasks,Tasks,Users;
+drop table if exists Attached_Tasks,Tasks,Users;
 SET foreign_key_checks = 1;
 
 create table Users
@@ -13,26 +13,37 @@ create table Users
 
 create table Tasks
 (
-	id int not null	primary key auto_increment,
-	author_id int,
+	id int not null	primary key auto_increment,	
 	name varchar(16),
-	foreign key (author_id) references Users(id)	
+	description text,
+	expiration_date DateTime	
 );
 
-create table Attached_tasks
+create table Attached_Tasks
 (
 	id int not null primary key auto_increment,
 	user_id int,
-	task_id int,
+	task_id int,	
+	creator_id int,
+	foreign key (creator_id) references Users(id),
 	foreign key (user_id) references Users(id),
 	foreign key (task_id) references Tasks(id)
 );
 
 
+-- test USERS --
 insert into Users(name,surname,age) 
-values(
-	'kekich',
-	'keknuty',
-	32
+values('Kekich','Testovy',1337),('Loh','zhmihovy',228);
+
+
+-- test TASKS
+insert into Tasks(name, description, expiration_date) values
+(
+	'Keknyt',
+	'zhestko nakekat',
+	NOW()
 );
 
+-- test many to many tasks
+insert into Attached_Tasks(user_id, task_id, creator_id) values
+(2,1,1),(1,1,1);
